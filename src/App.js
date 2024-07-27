2e42ca2eba239374686b241992d9f14944fc92a8
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2024-07-27 09:40:06 -0500

    npm i axios

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import { HelmetProvider } from 'react-helmet-async';
 import Router from './routes/routes';
 // theme
 import ThemeProvider from './theme';
+import axios from 'axios';
+// Set config defaults when creating the instance
+const instance = axios.create({
+  baseURL: 'http://localhost:8080/api/v1/',
+});
 
+// Alter defaults after instance has been created
+instance.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
 // ----------------------------------------------------------------------
 
 export default function App() {
