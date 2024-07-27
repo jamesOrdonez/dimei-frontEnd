198df67bd002d5f198ba1ff2788dc0ce4200651c
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2024-07-27 11:21:31 -0500

    modules

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import Swealert from '../../components/Swealert';
 import { LoaderModule } from '../../components/loaders';
 
-const data = [
+/* const data = [
   {
     id: 1,
     nombre: 'Jhon Mario Chilito',
@@ -85,27 +85,42 @@ const data = [
     fecha: '07/07/24',
   },
 ];
-
+ */
 export default function UserPage() {
   const [error, setError] = useState(false);
+  const [message, setMessage] = useState('');
+  const [loader, setLoader] = useState(true);
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     const AllUser = async () => {
       try {
         const respon = await axios.get('/getUser');
+        setData(respon.data);
+        setLoader(false);
       } catch (error) {
-        return setError(true);
+        setMessage(error.response.data.message);
+        setError(true);
+        return setLoader(false);
       }
     };
     AllUser();
   }, []);
-
+  if (loader) {
+    return (
+      <div class="bg-gray-200 rounded-full w-full max-w-4xl mx-auto mt-4">
+        <div class="w-1/2 py-0.5 flex items-center justify-center rounded-full bg-blue-500">
+          <p class="text-xs text-white font-bold leading-none">50%</p>
+        </div>
+      </div>
+    );
+  }
   return (
     <>
       <Helmet>
         <title> User | DIMEI </title>
       </Helmet>
-      <LoaderModule />
-      {/*  <DataGrid datos={data} error={true} message={'sapo'} /> */}
+      <DataGrid datos={data} error={error} message={message} />
     </>
   );
 }
