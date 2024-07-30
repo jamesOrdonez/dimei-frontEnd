7e7d775fb5ed4c86444936c03573f6e1d93dff17
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2024-07-30 18:06:20 -0500

    fix loader

--- src/components/loaders/loader/index.jsx
+++ src/components/loaders/loader/index.jsx
@@ -9,7 +9,7 @@ export default function Loader() {
         justifyContent: 'center',
       }}
     >
-      <div class="loaders" style={{ position: 'relative', right: '30px', color: 'gray' }} />{' '}
+      <div class="loaders" style={{ position: 'relative', right: '30px', color: '#c5c5c5' }} />
       <div class="loaders" style={{ position: 'absolute' }} />
     </div>
   );
