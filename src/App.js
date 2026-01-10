d0c12001d6b39031ad8d4caed6cbe571972a1454
whoaomi59 <whoaomi11@gmail.com>
2026-01-09 22:19:41 -0500

    url

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import ThemeProvider from './theme';
 import axios from 'axios';
 
 const TOKEN = sessionStorage.getItem('Token');
-axios.defaults.baseURL = 'http://localhost:8080/api/v1/';
-/* axios.defaults.baseURL = 'https://vps.equiposdimei.com/api/v1/'; */
+/* axios.defaults.baseURL = 'http://localhost:8080/api/v1/'; */
+axios.defaults.baseURL = 'https://vps.equiposdimei.com/api/v1/';
 axios.defaults.headers.common['Authorization'] = TOKEN;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 // ----------------------------------------------------------------------
